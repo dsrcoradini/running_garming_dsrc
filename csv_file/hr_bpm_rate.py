07b5c479f08c4e2c505f6cb1a69476abcb048447
Diego Santa Rosa Coradini <diegosantarosacoradini@gmail.com>
2025-12-12 15:58:33 +0100

    More stuff

--- csv_file/hr_bpm_rate.py
+++ csv_file/hr_bpm_rate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import os 
+import seaborn as sns
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -33,4 +33,37 @@ def finding_zones(hr_max, dic_data_list):
         item["Dfs"] = df #updating back into the dictionary
     return dic_data_list
 
-finding_zones(200,dic_data_list)
+finding_zones(200,dic_data_list) 
+
+#organizing zones as percentages overall the exercises
+
+#function to filter only the wished dataset. Distance needs to be written in meter 
+def run_df(dic_data_list, dis_max, dis_min):
+    results = []
+    for item in dic_data_list: 
+        if dis_min <= item["Distance"] <= dis_max:
+            results.append(item["Dfs"])
+    return results
+
+results = run_df(dic_data_list,16000,14000)
+#plotting an histogram
+
+def hist_run_hrzone(results):
+    zone_order = ["zone_1","zone_2","zone_3","zone_4","zone_5"]
+    colors = ["red","blue","green","orange","purple","cyan","magenta","brown","gray","olive"]
+
+    for idx, daframe in enumerate(results):
+        counts = daframe["hrzone"].value_counts().reindex(zone_order, fill_value=0)
+        temp = counts.reset_index()
+        temp.columns = ["hrzone", "count"]
+        temp["run"] = f"Run {idx+1}"
+        #plt grouped bars
+        sns.barplot(x="hrzone", y="count", data=temp, label=f"Run {idx+1}", order=zone_order,color= colors[idx%len(colors)], edgecolor="black")
+    plt.title("Heart Rate Zone Comparison Across Runs")
+    plt.xlabel("Heart Rate Zone")
+    plt.ylabel("Count (≈ time)")
+    plt.legend(title = "Run", ncol=2) #legend with multiple column for readability
+    plt.show()
+
+
+hist_run_hrzone(results)
